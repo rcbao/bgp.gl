9eca3e55edbb09643a8c13f91f9fb028eacf1c8a
robert_bao <ibaochen@gmail.com>
2024-03-21 17:35:39 -0400

    Restructure backends

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import os
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .components.map_data_connector import MapDataConnector
 
-# Create your views here.
+
+def internal_server_error():
+    error_response = {"error": "Internal Server Error"}
+    error_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    return Response(error_response, status=error_code)
+
+
+class USDataView(APIView):
+    """The API view for the entire US data"""
+
+    def post(self, request, *args, **kwargs):
+        try:
+            # Build the initial prompt using OpeningPromptBuilder
+            map_data = MapDataConnector().get_us_data()
+            widget_data = {}
+            us_data_response = {"map_data": map_data, "widget_data": widget_data}
+
+            return Response(us_data_response, status=status.HTTP_200_OK)
+
+        except ValueError as ve:
+            print(str(ve))
+            return Response({"error": str(ve)}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            print(e)
+            return internal_server_error()
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from api.views import USDataView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("us-overview/", USDataView.as_view(), name="us-overview"),
 ]
