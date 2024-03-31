d63486e45c3e39ce0aeb7d8343ae032a5f0615b5
LeafyJade <jade.gregoire2021@gmail.com>
2024-03-30 22:44:02 -0400

    sample code just for states and lat/long counts based on physical router ip

--- data_infra/rib_txt_to_json.py
+++ data_infra/rib_txt_to_json.py
@@ -1,10 +1,10 @@
 import json
-import geocoder # Needs pip install geocoder --user
+import geocoder # Needs pip install geocoder --user : https://geocoder.readthedocs.io/providers/IPInfo.html
 
 # Function to process the rib.txt file
 def process_rib_file(rib_file_path):
     
-    us_json = {}
+    us_json = {"overview": {}, "charts": {"prefixLengthDistribution": [], "usAnnouncementHeatMap": {}}}
     states_json = {}
 
     # Read the rib.txt file and process each line
@@ -19,14 +19,32 @@ def process_rib_file(rib_file_path):
             # Get location
             location = geocoder.ip(physical_router_address)
             if location.country == "US":
-                lat_long_arr = location.latlng
+                # lat_long_arr = location.latlng
+                latitude = location.lat
+                longitude = location.lng
                 state = location.state
+
+                if state in states_json:
+                    for item in states_json[state]["charts"]["stateAnnouncementHeatMap"]:
+                        if item.get('long') == longitude and item.get('lat') == latitude:
+                            # Update the "count" value for the found item
+                            item['count'] = item.get("count", 0) + 1
+                            break
+                    else: 
+                        states_json[state]["charts"]["stateAnnouncementHeatMap"].append({"long": longitude, "lat": latitude, "count": 1})
+                else: 
+                    states_json[state] = {"overview": {}, "charts": {"prefixLengthDistribution": [], "stateAnnouncementHeatMap": [{"long": longitude, "lat": latitude, "count": 1}]}}
+                
+                if state in us_json["charts"]["usAnnouncementHeatMap"]: 
+                    us_json["charts"]["usAnnouncementHeatMap"][state] += 1
+                else: 
+                    us_json["charts"]["usAnnouncementHeatMap"][state] = 1
             
     return us_json, states_json
 
 # Main function to run the script
 def main():
-    rib_file_path = "rib.20240321.2000.tail.size.500.txt" # Change this to RIB txt file
+    rib_file_path = "sample_rib.txt" # Change this to RIB txt file
     us_json, states_json = process_rib_file(rib_file_path)
 
     # Write the JSON data to a file
