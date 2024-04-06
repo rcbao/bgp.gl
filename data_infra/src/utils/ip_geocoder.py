a90dd730df063cbc1916c0dc817d5a6c53ecbe7d
robert_bao <ibaochen@gmail.com>
2024-04-06 16:50:16 -0400

    Update

--- data_infra/src/utils/ip_geocoder.py
+++ data_infra/src/utils/ip_geocoder.py
@@ -1,18 +1,15 @@
 import ipaddress
 import sqlite3
 from .constants import IP_DATABASE
+from functools import lru_cache
 
 
 class IPGeocoder:
     def __init__(self, db_file=IP_DATABASE):
         self.conn = sqlite3.connect(db_file)
         self.cursor = self.conn.cursor()
-        self.cache = {}  # Add a simple cache
 
     def lookup_ip_coordinates(self, ip_str):
-        if ip_str in self.cache:
-            return self.cache[ip_str]
-
         ip_int = int(ipaddress.ip_address(ip_str))
         self.cursor.execute(
             """SELECT latitude, longitude, state1 FROM ip_ranges
@@ -21,8 +18,7 @@ class IPGeocoder:
         )
         result = self.cursor.fetchone()
         if not result:
-            result = (None, None, None)
-        self.cache[ip_str] = result  # Cache the result
+            return (None, None, None)
         return result
 
     def close(self):
