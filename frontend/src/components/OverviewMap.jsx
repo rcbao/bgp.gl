40db8240f2877684bc09aa10378b8cf847dc3277
robert_bao <ibaochen@gmail.com>
2024-04-07 15:39:49 -0400

    Map part 1

--- frontend/src/components/OverviewMap.jsx
+++ frontend/src/components/OverviewMap.jsx
@@ -3,16 +3,6 @@ import usStatesGeoJson from "./us-states.json";
 import DeckGLMap from "./DeckGLMap";
 import { fillColors } from "./utils/constants";
 
-const getTooltip = ({ object }) => {
-    if (!object) {
-        return null;
-    }
-    const stateName = object.properties.name;
-    const stateAbbr = object.properties.abbreviation;
-
-    return `${stateName} (${stateAbbr.toUpperCase()})`;
-};
-
 export const overviewViewState = {
     longitude: -97,
     latitude: 40,
@@ -20,14 +10,54 @@ export const overviewViewState = {
     maxZoom: 10,
 };
 
-const OverviewMap = ({ data, navigate }) => {
+function getColorFromAnnouncements(announcements) {
+    // This is a simple example - you might want to implement a more complex function
+    // that maps a range of announcement counts to different colors.
+    if (announcements < 10000) {
+        return [221, 221, 221, 200];
+    } else if (announcements < 50000) {
+        return [167, 201, 232, 200];
+    } else if (announcements < 100000) {
+        return [85, 150, 213, 200];
+    } else {
+        return [60, 103, 150, 200]; // Green for 0 announcements
+    }
+}
+
+const OverviewMap = (props) => {
+    const data = props.data;
+    const navigate = props.navigate;
+
+    if (!data || !data["usAnnouncementHeatMap"]) {
+        console.log("Waiting for data...");
+        return null; // Return null or a loading indicator while data is being fetched
+    }
+
+    const getTooltip = ({ object }) => {
+        if (!object) {
+            return null;
+        }
+        const stateName = object.properties.name;
+        const stateAbbr = object.properties.abbreviation;
+        const announcements = data["usAnnouncementHeatMap"][stateName];
+
+        return `${stateName} (${stateAbbr.toUpperCase()})\n ${
+            announcements || 0
+        } announcements`;
+    };
+
     const geoJsonLayer = new GeoJsonLayer({
         id: "geojson-layer",
         data: usStatesGeoJson,
         stroked: true,
         filled: true,
         lineWidthMinPixels: 2,
-        getFillColor: fillColors.default,
+        getFillColor: (feature) => {
+            const stateName = feature.properties.name; // Adjust based on your GeoJSON structure
+            const announcements = data["usAnnouncementHeatMap"][stateName] || 0; // Fallback to 0 if no data
+            // Implement your logic to map `announcements` to a color
+            return getColorFromAnnouncements(announcements);
+        },
         getLineColor: [0, 0, 0, 255],
         autoHighlight: true,
         highlightColor: fillColors.highlight,
@@ -42,11 +72,13 @@ const OverviewMap = ({ data, navigate }) => {
 
     const layers = [geoJsonLayer];
     return (
-        <DeckGLMap
-            layers={layers}
-            viewState={overviewViewState}
-            getTooltip={getTooltip}
-        />
+        data && (
+            <DeckGLMap
+                layers={layers}
+                viewState={overviewViewState}
+                getTooltip={getTooltip}
+            />
+        )
     );
 };
 export default OverviewMap;
--- frontend/src/components/OverviewMapPage.jsx
+++ frontend/src/components/OverviewMapPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "maplibre-gl/dist/maplibre-gl.css";
 import OverviewMap from "./OverviewMap";
@@ -6,52 +6,80 @@ import OverviewMap from "./OverviewMap";
 const OverviewMapPage = () => {
     const navigate = useNavigate();
 
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`http://127.0.0.1:8000/us-overview/`, {
+                // posts the form to users/me/items. You need to login to be able to send this
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            const mapData = await response.json();
+            if (response.ok) {
+                setData(mapData);
+                console.log(mapData);
+            } else {
+                // if cannot access user info: show an error message
+                navigate(`/`);
+            }
+        };
+
+        fetchData();
+    }, [navigate]);
+
     return (
-        <div className="columns-2 flex flex-row">
-            <OverviewMap navigate={navigate} data={[]} />
-            <div style={{ width: "40vw" }} className="p-10">
-                <h1 className="text-3xl font-bold">
-                    United States BGP Traffic Map
-                </h1>
-                <div className="my-6">
-                    <h2 className="text-xl font-bold my-4">
-                        High-level Overview
-                    </h2>
-                    <div className="grid grid-cols-2 gap-4">
-                        <div className="dashboard-widget-card ">
-                            <h5 className="dashboard-widget-card-header">
-                                BGP Announcements
-                            </h5>
-                            <h3 className="dashboard-widget-card-data">
-                                {128000}
-                            </h3>
-                        </div>
-                        <div className="dashboard-widget-card">
-                            <h5 className="dashboard-widget-card-header">
-                                Most Advertised Prefix
-                            </h5>
-                            <h3 className="dashboard-widget-card-data">
-                                127.0.0.1
-                            </h3>
-                        </div>
-                        <div className="dashboard-widget-card">
-                            <h5 className="dashboard-widget-card-header">
-                                Org with Most Routes
-                            </h5>
-                            <h3 className="dashboard-widget-card-data">
-                                Google Inc.
-                            </h3>
-                        </div>
-                        <div className="dashboard-widget-card">
-                            <h5 className="dashboard-widget-card-header">
-                                Most Advertised Prefix Length
-                            </h5>
-                            <h3 className="dashboard-widget-card-data">12</h3>
+        data && (
+            <div className="columns-2 flex flex-row">
+                <OverviewMap navigate={navigate} data={data} />
+                <div style={{ width: "40vw" }} className="p-10">
+                    <h1 className="text-3xl font-bold">
+                        United States BGP Traffic Map
+                    </h1>
+                    <div className="my-6">
+                        <h2 className="text-xl font-bold my-4">
+                            High-level Overview
+                        </h2>
+                        <div className="grid grid-cols-2 gap-4">
+                            <div className="dashboard-widget-card ">
+                                <h5 className="dashboard-widget-card-header">
+                                    BGP Announcements
+                                </h5>
+                                <h3 className="dashboard-widget-card-data">
+                                    {128000}
+                                </h3>
+                            </div>
+                            <div className="dashboard-widget-card">
+                                <h5 className="dashboard-widget-card-header">
+                                    Most Advertised Prefix
+                                </h5>
+                                <h3 className="dashboard-widget-card-data">
+                                    127.0.0.1
+                                </h3>
+                            </div>
+                            <div className="dashboard-widget-card">
+                                <h5 className="dashboard-widget-card-header">
+                                    Org with Most Routes
+                                </h5>
+                                <h3 className="dashboard-widget-card-data">
+                                    Google Inc.
+                                </h3>
+                            </div>
+                            <div className="dashboard-widget-card">
+                                <h5 className="dashboard-widget-card-header">
+                                    Most Advertised Prefix Length
+                                </h5>
+                                <h3 className="dashboard-widget-card-data">
+                                    12
+                                </h3>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        )
     );
 };
 
